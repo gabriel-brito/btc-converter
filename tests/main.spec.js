26f1101fea28cce5ba90f9368d9591612a10b1ed
Gabriel Santos <gabrielsantos@ext.cvccorp.com.br>
2019-03-26 15:35:45 -0300

    Starting CLI development

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,24 @@
 import { expect } from 'chai';
+import { exec } from 'child_process';
 
-describe('Main', () => {
+const btcConverter = './src/main.js';
+const pkg = require('../package.json');
 
+describe('Main CLI', () => {
+  it('should return version of btc-converter', (done) => {
+    exec(`${btcConverter} --version`, (err, stdout, stderr) => {
+      if(err) throw err;
+      expect(stdout.replace('\n', '')).to.be.equal(pkg.version);
+      done();
+    })
+  });
+
+  it('should return the description message', (done) => {
+    exec(`${btcConverter} --help`, (err, stdout, stderr) => {
+      if(err) throw err;
+      const text = 'Convert Bitcoin to any currency defined.';
+      expect(stdout.includes(text)).to.be.true;
+      done();
+    })
+  });
 });
