174f68b81c8d35de644913a6442870437cc9c314
Gabriel Santos <gabrielsantos@ext.cvccorp.com.br>
2019-03-27 09:57:32 -0300

    Finishing BTC-Converter and adding async/await

--- src/ConvertBTC.js
+++ src/ConvertBTC.js
@@ -1,5 +1,18 @@
+const request = require('request-promise-native');
+
 function convertBTC(currency = 'USD', amount = 1) {
-  return `${amount} BTC to ${currency} = 2000,00`;
+  const url = `https://apiv2.bitcoinaverage.com/convert/global?from=BTC&to=${currency}&amount=${amount}`;
+
+  return request(url)
+    .then(body => body)
+    .then(body => {
+      const apiResponse = JSON.parse(body);
+      console.info(`${amount} BTC to ${currency} = ${apiResponse}`);
+    })
+    .catch(error => {
+      console.info('Something went wrong. Try again later.');
+      return error;
+    })
 }
 
- module.exports = convertBTC;
+module.exports = convertBTC;
--- tests/ConvertBTC.spec.js
+++ tests/ConvertBTC.spec.js
@@ -8,39 +8,44 @@ const convertBTC = require('../src/ConvertBTC.js');
 chai.use(sinonChai);
 
 describe('convertBTC', () => {
-
   let consoleStub;
 
   const responseMock = {
     "time": "2017-07-02 18:51:29",
     "price": 2490.78,
     "success": true
-  }
+  };
 
   beforeEach(() => {
-    console.consoleStub = sinon.stub(console, 'log');
+    consoleStub = sinon.stub(console, 'info');
   });
 
   afterEach(() => {
-    console.log.restore();
+    consoleStub.restore();
   });
 
-  it('should return USD as currency and 1 as amount default', (done) => {
+  it('should return USD as currency and 1 as amount default', async () => {
     // https://apiv2.bitcoinaverage.com/convert/global?from=BTC&to=USD&amount=1
     nock('https://apiv2.bitcoinaverage.com')
       .get('/convert/global')
       .query({ from: 'BTC', to: 'USD', amount: 1 })
       .reply(200, responseMock);
 
-    convertBTC();
+    await convertBTC();
+    expect(consoleStub).to.have.been
+      .calledWith('1 BTC to USD = 2490.78');
+  });
 
-    setTimeout(() => {
-      expect(consoleStub).to.have.been
-        .calledWith('1 BTC to USD = 2490.78');
-      done();
-    }, 300)
+  it('should return an error message when gone wrong', async () => {
+    nock('https://apiv2.bitcoinaverage.com')
+      .get('/convert/global')
+      .query({ from: 'BTC', to: 'USD', amount: 1 })
+      .replyWithError('Error');
 
+    await convertBTC();
 
+    expect(consoleStub).to.have.been
+      .calledWith('Something went wrong. Try again later.');
   });
 
 });
